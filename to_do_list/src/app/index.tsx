d279c9b77b31dfbbbb4fc50131932d809409831c
ruqayah22 <ruqaya.alnawfaly@outlook.com>
2025-07-14 11:48:01 +0300

    for inputs data make it as interface not type to show if there any difference between them

--- to_do_list/src/app/index.tsx
+++ to_do_list/src/app/index.tsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 import { useForm, type SubmitHandler } from "react-hook-form";
 
-type Inputs = {
+// type Inputs = {
+//   text: string;
+// };
+
+interface IFormInput {
   text: string;
-};
+}
+  
 
 export default function App() {
-  const {
-    register,
-    handleSubmit,
-    // watch,
-    // formState: { errors },
-    reset,
-  } = useForm<Inputs>(
-    // {
-//     defaultValues: {
-//       text: "",
-//     },
-//   }
-);
+//   const {
+//     register,
+//     handleSubmit,
+//     // watch,
+//     // formState: { errors },
+//     reset,
+//   } = useForm<Inputs>(
+//     // {
+// //     defaultValues: {
+// //       text: "",
+// //     },
+// //   }
+// );
+const { register, handleSubmit, reset } = useForm<IFormInput>();
+
   //   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   //   console.log(watch("text")); // watch input value by passing the name of it
@@ -27,11 +34,12 @@ export default function App() {
 
   const [items, setItems] = useState<string[]>([]);
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    setItems((prev) => [...prev, data.text]);
-    // console.log(data.text);
-    reset();
-  };
+//   const onSubmit: SubmitHandler<Inputs> = (data) => {
+    const onSubmit: SubmitHandler<IFormInput> = (data) => {
+      setItems((prev) => [...prev, data.text]);
+      // console.log(data.text);
+      reset();
+    };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
